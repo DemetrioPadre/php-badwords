7891cde547cf5ccc76bcd51f2bbe80b47c8dfc76
DemetrioPadre <demetriopadre@gmail.com>
2024-02-24 21:09:13 +0100

    ex completed

--- result.php
+++ result.php
@@ -1,19 +1,46 @@
-<?php
- $greeting= "ciao padrone"; 
- $congratulations="sono qua per servirti";
- $fullpower= $greeting .$congratulations
- ?>
 
+<?php
+$user_text= $_GET['name'];
+$bad_word= $_GET['bad_word'];
+// $bad_word_lenght = strlen($bad_word);
+$new_bad_word = str_replace($bad_word,'***', $bad_word );
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "primo esercizio in php" ?></title>
+    <title>Document</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> 
 </head>
 <body>
-    <h1> <?php echo $fullpower?></h1>
-  
+<div class="container mt-5">
+    <div class="firstW"><h1> Ecco la stampa della parola da te scelta e/o inserita : "
+    <?php   echo $user_text;?>
+    "
+    </h1>
+
+<h2> la tua parola è lunga:  <?php echo strlen($user_text)?> caratteri </h2>
+</div>
+    
+    <div class="badWord">
+        <h3>vorrei censurare la parola  : " <?php echo $bad_word ?> " , che ha <?php echo strlen($bad_word) ?> caratteri,  sarebbe possibile?</h3>
+<h4> 
+    Ecco la tua parola censurata : "
+    <?php echo $new_bad_word  ?> " , che ha <?php echo strlen($bad_word)?> caratteri </h4>
+    
+
+
+    </div>
+    
+   
+    
+    
+    
+        
+    </div>
+    
 </body>
 </html>
