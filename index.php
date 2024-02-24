29d4a84f577bb947485a77ad4e13d9b19c2c243e
DemetrioPadre <demetriopadre@gmail.com>
2024-02-24 17:17:20 +0100

    add hello world

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-
+<?php
+ $greeting= "ciao padrone"; 
+ $congratulations="ora finalmente sei un front-ender, e adesso puoi iniziare la seconda parte del corso che hai pagato 5 maledettissimi K"
+ ?>
 
 
 <!DOCTYPE html>
@@ -6,9 +9,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo "primo esercizio in php" ?></title>
 </head>
 <body>
-    <h1><?php echo "hello world" ?></h1>
+    <h1><?php echo $greeting ?></h1>
+
+    <h2><?php echo $congratulations?></h2>
 </body>
 </html>
