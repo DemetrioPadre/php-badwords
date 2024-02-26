203a5f5f24aa39d42cb98d90632df4d894a30e87
DemetrioPadre <demetriopadre@gmail.com>
2024-02-26 16:46:24 +0100

    exercise fixed

--- result.php
+++ result.php
@@ -4,7 +4,7 @@ $user_text= $_GET['name'];
 $bad_word= $_GET['bad_word'];
 //variabile che serve a inserire tanti * quanto lunga la parola da censurare, grazie signor google e php docs
 $bad_word_m = str_repeat('*',strlen($bad_word) );
-$new_bad_word = str_replace($bad_word, $bad_word_m , $bad_word);
+$new_bad_word = str_replace($bad_word, $bad_word_m , $user_text);
 ?>
 
 <!DOCTYPE html>
