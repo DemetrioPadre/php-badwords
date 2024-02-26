e396b974a7392c5299a418f07292987413d15530
DemetrioPadre <demetriopadre@gmail.com>
2024-02-26 16:53:20 +0100

    add include_once

--- result.php
+++ result.php
@@ -1,10 +1,6 @@
 
 <?php
-$user_text= $_GET['name'];
-$bad_word= $_GET['bad_word'];
-//variabile che serve a inserire tanti * quanto lunga la parola da censurare, grazie signor google e php docs
-$bad_word_m = str_repeat('*',strlen($bad_word) );
-$new_bad_word = str_replace($bad_word, $bad_word_m , $user_text);
+include_once("./init.php")
 ?>
 
 <!DOCTYPE html>
